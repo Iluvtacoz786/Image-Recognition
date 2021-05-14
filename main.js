9af5d32fccfa901665b3e5aefc982d5de6ad9bbc
Zoya Rana <74219818+Iluvtacoz786@users.noreply.github.com>
2021-05-13 18:45:42 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -10,3 +10,25 @@ function capture(){
         document.getElementById("snapshot").innerHTML="<img src='"+data_URI+"' id='captured_image'>";
     })
 }
+console.log(ml5.version);
+var classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/SYklLe9bL/model.json", modelLoaded)
+function modelLoaded(){
+    console.log("Model is working");
+}
+function identify(){
+    captured_image=document.getElementById("captured_image");
+    classifier.classify(captured_image, gotResult);
+}
+function gotResult(error, result){
+if(error){
+    console.log(error)
+}
+else{
+    console.log(result)
+    var object=result[0].label;
+    var accuracy=result[0].confidence;
+    document.getElementById("object name").innerHTML=object;
+    document.getElementById("accuracy").innerHTML=accuracy;
+
+}
+}
